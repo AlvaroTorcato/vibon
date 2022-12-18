cd8b4424b62e0aac0c924e9fa42b09aa24f027bf
Álvaro Torcato <alvaromanuelreis@gmail.com>
2022-12-18 16:36:32 +0000

    alpha e beta

--- src/main/java/com/example/vibon/Model/Calculos.java
+++ src/main/java/com/example/vibon/Model/Calculos.java
@@ -1,4 +1,18 @@
 package com.example.vibon.Model;
 
 public class Calculos {
+
+    public double calcularAlpha(double temp, double[] time_ms, double[] dist_mm){
+        double alpha = 0;
+        double tempK= (float) (temp + 273.15);
+        LinearRegression linearRegression=new LinearRegression(time_ms, dist_mm);
+        return alpha=linearRegression.slope();
+    }
+
+    public double calcularBeta(double temp, double[] time_ms, double[] dist_mm){
+        double alpha = 0;
+        double tempK= (float) (temp + 273.15);
+        LinearRegression linearRegression=new LinearRegression(time_ms, dist_mm);
+        return alpha=linearRegression.intercept();
+    }
 }
