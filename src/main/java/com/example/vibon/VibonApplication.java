96695c657ec3adb26a03adad6a0fe4a35f6e0c84
Álvaro Torcato <alvaromanuelreis@gmail.com>
2022-12-18 16:11:54 +0000

    Initial commit

---
+++ src/main/java/com/example/vibon/VibonApplication.java
@@ -0,0 +1,13 @@
+package com.example.vibon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VibonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VibonApplication.class, args);
+	}
+
+}
